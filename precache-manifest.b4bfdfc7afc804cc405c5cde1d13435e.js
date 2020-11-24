1b7a41964683677a9a90fad7a433a7770ccb51b7
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-11-24 22:04:01 +0100

    Updates

---
+++ precache-manifest.b4bfdfc7afc804cc405c5cde1d13435e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b7f0ae13730f48acf1b73925fceff0",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9868beff74e1b7fbe331",
+    "url": "/weather-app/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "0a76afc19f2b1ed717a6",
+    "url": "/weather-app/static/css/main.d75384d6.chunk.css"
+  },
+  {
+    "revision": "9868beff74e1b7fbe331",
+    "url": "/weather-app/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "0a76afc19f2b1ed717a6",
+    "url": "/weather-app/static/js/main.5fa70cbf.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "576b52f84f07e8ae4d682e60d495729f",
+    "url": "/weather-app/static/media/bg.576b52f8.jpg"
+  }
+]);
